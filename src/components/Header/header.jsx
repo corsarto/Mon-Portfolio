0a80ae8bc402db1b047ead4ddea4b6a7d0af42d6
corsarto <clerc.loic@gmail.com>
2025-09-08 13:59:16 +0200

    change nav bar for mobile and fix responsive

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,23 +1,50 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../Header/header.css";
 
 function Header() {
+  const [isNavOpen, setisNavOpen] = useState(false);
+
   return (
     <div className="header">
       <Link to="/" className="title-link">
         Loïc Clerc
       </Link>
-      <nav className="nav">
-        <a href="#about" className="nav-link">
+      <div
+        className={`nav-toggle ${isNavOpen ? "active" : ""}`}
+        onClick={() => setisNavOpen(!isNavOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`nav ${isNavOpen ? "active" : ""}`}>
+        <a
+          href="#about"
+          className="nav-link"
+          onClick={() => setisNavOpen(false)}
+        >
           À propos
         </a>
-        <a href="#skills" className="nav-link">
+        <a
+          href="#skills"
+          className="nav-link"
+          onClick={() => setisNavOpen(false)}
+        >
           Mes compétences
         </a>
-        <a href="#projects" className="nav-link">
+        <a
+          href="#projects"
+          className="nav-link"
+          onClick={() => setisNavOpen(false)}
+        >
           Projets
         </a>
-        <a href="mailto:clerc.loic@gmail.com" className="nav-link">
+        <a
+          href="mailto:clerc.loic@gmail.com"
+          className="nav-link"
+          onClick={() => setisNavOpen(false)}
+        >
           Contact
         </a>
       </nav>
